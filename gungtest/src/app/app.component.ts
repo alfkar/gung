f17274a0b90e30a6c17abd1fcbc1b74243944453
alfkar <alfkar@student.chalmers.se>
2024-03-03 12:48:28 +0100

    using service

--- gungtest/src/app/app.component.ts
+++ gungtest/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TableComponent } from './table/table.component';
+import { Product, ProductService } from './product.service';
+import { Observable, forkJoin } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, TableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ProductService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gungtest';
+  products: Observable<Product[]>;
+  numberOfRandomProducts = 5; 
+  constructor(private productService:ProductService){
+
+  }
+  ngOnInit() {
+    // Call getRandomProduct() multiple times and combine the results into an array
+    const randomProductObservables: Observable<Product>[] = [];
+    for (let i = 0; i < this.numberOfRandomProducts; i++) {
+      randomProductObservables.push(this.productService.getRandomProduct('someId')); // Pass a sample id
+    }
+
+    // Combine multiple observables into one observable array
+    this.products = forkJoin(randomProductObservables);
+  }
 }
