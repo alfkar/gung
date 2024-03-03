5b0ce532c5865a9536c1faac90c64a8cd8e75803
alfkar <alfkar@student.chalmers.se>
2024-03-04 00:13:37 +0100

    displaying the categories properly

--- gungtest/src/app/app.component.ts
+++ gungtest/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService, Product } from './product.service';
+import { CategoryService, Category } from './category.service';
 import { Observable, forkJoin } from 'rxjs';
 import { TableComponent } from './table/table.component';
 
@@ -8,29 +9,42 @@ import { TableComponent } from './table/table.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ProductService],
+  providers: [ProductService,CategoryService],
   imports: [TableComponent]
 })
 export class AppComponent implements OnInit {
   title = 'gungtest';
-  products: Product[] = []; // Initialize products as an empty array
+  prod: ProductWithCategory;
+  products: ProductWithCategory[] = []; // Initialize products as an empty array
+  categories: Observable<Category>;
   id = "";
   numberOfRandomProducts = 5;
 
-  constructor(private productService: ProductService) {}
-
-  ngOnInit() {
-    const randomProductObservables: Observable<Product>[] = [];
-    for (let i = 0; i < this.numberOfRandomProducts; i++) {
-      this.id = "" + i;
-      randomProductObservables.push(this.productService.getRandomProduct(this.id));
-    }
-
-    forkJoin(randomProductObservables)
-      .subscribe((products: Product[]) => {
-        this.products = products; // Assign the array of products
-      });
+  constructor(private productService: ProductService, private categoryService: CategoryService) {
+    this.categories = this.categoryService.getCategories();
   }
-
+  ngOnInit() {
+    this.categories.subscribe(categories => {
+      this.flattenProducts(categories, []);
+    });
+  }
+  flattenProducts(category: Category, parentCategory: string[] = []) {
+    if(category.id.charAt(0) == 's'){
+      if(category.children.length > 0){
+        category.children.forEach(child => {
+        this.flattenProducts(child, parentCategory.concat([category.id]));
+      })
+      }
+    }
+    else{
+    this.productService.getProduct(category.id).subscribe(product => {
+        this.prod = { ...product, category: parentCategory }; // Initialize this.prod before assigning values
+        this.products.push(this.prod);
+        console.log(this.prod.category)
+      });    
+    }
+  }
 }
-
+export interface ProductWithCategory extends Product{
+  category?: string[];
+}
