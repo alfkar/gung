2e53f0c70373b997a63f9ed687420d24c2baf087
Alfred Karlsson <alfred_karlsson@hotmail.com>
2024-03-05 17:28:42 +0100

    added paging and started on sorting volume

--- gungtest/src/app/app.component.ts
+++ gungtest/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService, Product } from './product.service';
 import { CategoryService, Category } from './category.service';
+import { ScrollingModule, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { Observable, forkJoin } from 'rxjs';
 import { TableComponent } from './table/table.component';
 
@@ -9,7 +10,7 @@ import { TableComponent } from './table/table.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ProductService,CategoryService],
+  providers: [ProductService,CategoryService,ScrollingModule, CdkVirtualScrollViewport],
   imports: [TableComponent]
 })
 export class AppComponent implements OnInit {
@@ -22,29 +23,43 @@ export class AppComponent implements OnInit {
 
   constructor(private productService: ProductService, private categoryService: CategoryService) {
     this.categories = this.categoryService.getCategories();
+    //this.categories = this.categoryService.getAlotOfCategories();
   }
   ngOnInit() {
     this.categories.subscribe(categories => {
-      this.flattenProducts(categories, []);
+      this.flattenProducts(categories, [], "");
     });
   }
-  flattenProducts(category: Category, parentCategory: string[] = []) {
+  flattenProducts(category: Category, parentCategory: string[] = [], categoryNames: string) {
     if(category.id.charAt(0) == 's'){
       if(category.children.length > 0){
         category.children.forEach(child => {
-        this.flattenProducts(child, parentCategory.concat([category.id]));
+        this.flattenProducts(child, parentCategory.concat([category.id]), categoryNames + " " + category.name);
       })
       }
     }
     else{
     this.productService.getProduct(category.id).subscribe(product => {
-        this.prod = { ...product, category: parentCategory }; // Initialize this.prod before assigning values
-        this.products.push(this.prod);
-        console.log(this.prod.category)
-      });    
+        if (product != null){
+          this.prod = { ...product, categoryId: parentCategory,categoryName: categoryNames };
+          this.products.push(this.prod);
+        }
+        else{
+          this.prod = {
+            id: "0",
+            name: "TestName",
+            extra: {"AGA": {"APE": "      1","KAT": "TU","LGA": "       1.00","PRI": "    4875.00","TYP": "","VOL": "    5.300","VPE": "    0.000","XP1": "          0","XP2": "          0"}},
+            categoryId: parentCategory,
+            categoryName: categoryNames
+          }
+          this.products.push(this.prod);
+        }
+      });
     }
   }
 }
 export interface ProductWithCategory extends Product{
-  category?: string[];
+  categoryId?: string[];
+  categoryName?: string;
+
 }
--- gungtest/src/app/table/table.component.ts
+++ gungtest/src/app/table/table.component.ts
@@ -1,21 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Product } from '../product.service';
+import { Component, Input, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollingModule, CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { ProductWithCategory } from '../app.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
-  imports: [CommonModule]
+  changeDetection: ChangeDetectionStrategy.Default,
+  imports: [CommonModule, ScrollingModule, CdkVirtualScrollViewport]
 })
 export class TableComponent implements OnInit {
-  @Input() products: Product[] = []; // Receive the products array from the parent
+  @Input() products: ProductWithCategory[] = []; // Receive the products array from the parent
 
   constructor() {}
 
   ngOnInit() {
     // Use the received products array in the child component
     console.log('Received products:', this.products);
+  }
+  sortProductsByVolume() {
+    this.products.sort((a, b) => {
+      if (a.extra.AGA.VOL< b.extra.AGA.VOL) return -1;
+      if (a.extra.AGA.VOL > b.extra.AGA.VOL) return 1;
+      return 0;
+    });
+    console.log("Products should be sorted");
+    console.log(this.products);
   }
 }
